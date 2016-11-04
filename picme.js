acafb705805b11f8623bed32c1a1837fefa46269
John Knickerbocker <PokeMaster843@gmail.com>
2016-11-04 09:17:45 -0400

    Update picme.js

--- picme.js
+++ picme.js
@@ -2,6 +2,8 @@ var cnv = document.getElementById("mainPage");
 var ctx = cnv.getContext("2d");
 var grd = ctx.createLinearGradient(0, 0, 0, 160);
 
+cnv.width = window.innerWidth;
+cnv.height = window.innerHeight;
 grd.addColorStop(0, "#99b3ff");
 grd.addColorStop(0.5, "#0033cc");
 grd.addColorStop(1, "#99b3ff");
