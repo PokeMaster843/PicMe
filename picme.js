811452799f184e73c634246fd1217f889198b202
John Knickerbocker <PokeMaster843@gmail.com>
2016-11-04 08:50:57 -0400

    Update picme.js

--- picme.js
+++ picme.js
@@ -1,1 +1,13 @@
+var cnv = document.getElementById("site");
+var ctx = cnv.getContext("2d");
 
+var update = setInterval(function() {
+  
+  var grd = ctx.createLinearGradient(0, 0, 0, 160);
+  grd.addColorStop(0, "#99b3ff");
+  grd.addColorStop(0.5, "#0033cc");
+  grd.addColorStop(1, "#99b3ff");
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, cnv.width, 160);
+  
+}, 1000*2);
