f8b41ae1f9781ea98d134284b3564034da9fb0c9
John Knickerbocker <PokeMaster843@gmail.com>
2016-11-09 13:26:23 -0500

    Update picme.js

--- picme.js
+++ picme.js
@@ -10,3 +10,9 @@ grd.addColorStop(0.5, "#0033cc");
 grd.addColorStop(1, "#3366ff");
 ctx.fillStyle = grd;
 ctx.fillRect(0, 0, cnv.width - scrollbar, 80);
+ctx.strokeStyle = "#000000";
+
+var home = ctx.rect(0, 0, 120, 80);
+var profile = ctx.rect(120, 0, 120, 80);
+var settings = ctx.rect(240, 0, 120, 80);
+var friends = ctx.rect(360, 0, 120, 80);
