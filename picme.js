7acd5c6a7bd8d1631a35d5b910e42a2041193bbd
John Knickerbocker <PokeMaster843@gmail.com>
2016-11-09 12:57:22 -0500

    Update picme.js

--- picme.js
+++ picme.js
@@ -1,13 +1,11 @@
 var cnv = document.getElementById("mainPage");
 var ctx = cnv.getContext("2d");
-var grd = ctx.createLinearGradient(0, 0, 0, 160);
+var grd = ctx.createLinearGradient(0, 0, 0, 80);
 
-cnv.offsetLeft = 0;
-cnv.offsetTop = 0;
 cnv.width = window.innerWidth;
 cnv.height = window.innerHeight;
 grd.addColorStop(0, "#3366ff");
 grd.addColorStop(0.5, "#0033cc");
 grd.addColorStop(1, "#3366ff");
 ctx.fillStyle = grd;
-ctx.fillRect(0, 0, cnv.width, 160);
+ctx.fillRect(0, 0, cnv.width, 80);
